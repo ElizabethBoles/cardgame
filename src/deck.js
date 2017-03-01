29b04c671ae4b72351c67948d550f7007b032edc
elizabeth boles <elizabethboles@elizabeths-MacBook-Air.local>
2017-03-01 16:45:42 -0500

    added deck of cards

---
+++ src/deck.js
@@ -0,0 +1,17 @@
+//1) The deck.js file must export a single function which returns an array of shuffled card values.
+//TThere should be FOUR of each value:
+ //2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', and 'A'.
+ // Note that you DO NOT need to worry about suits (unless you do Adventure Mode).
+let deck = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ];
+
+module.exports = function buildDeck() {
+//let shuffleDeck = [];
+//let randoCard = shuffleDeck.push(deck[deckmath.floor(math.random() * deck.length());
+
+
+
+
+  return deck;
+};
+
+console.log(deck);
